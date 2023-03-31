e09bfdc6a8667fda930478a247ccf0bd6aa245a5
Timothy Axford <t.axford2@yahoo.com>
2023-03-31 09:18:44 -0400

    Added properties and a constructor to the TV class similar to the Radio class

--- Inheritance.Net5.0/TV.cs
+++ Inheritance.Net5.0/TV.cs
@@ -8,5 +8,20 @@ namespace Inheritance.Net5._0
 {
     internal class TV
     {
+        //boolean to determine the state of the tv
+
+        public bool Ison { get; set; }
+        //string for the brand name of the tv
+
+        public string Brand { get; set; }
+
+        //basic constructor
+
+        public TV(bool isOn, string brand)
+        {
+
+            Ison = isOn;
+            Brand = brand;
+        }
     }
 }
