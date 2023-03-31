e44e3987af6fd6086e36464f5a07027c5463ee93
Timothy Axford <t.axford2@yahoo.com>
2023-03-31 09:06:41 -0400

    Added 2 properties in the Radio class

--- Inheritance.Net5.0/Radio.cs
+++ Inheritance.Net5.0/Radio.cs
@@ -8,5 +8,11 @@ namespace Inheritance.Net5._0
 {
     internal class Radio
     {
+        //boolean to determine the state of the Radio
+
+        public bool Ison { get; set; }
+        //string for the brand name of the radio
+
+        public string Brand { get; set; }
     }
 }
