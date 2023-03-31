cb0c1bb00f30c5a8e9ce3e71c400e38d681499b6
Timothy Axford <t.axford2@yahoo.com>
2023-03-31 09:35:12 -0400

    Created a new class called Electrical Device which will be the class for everything to inherit the properties

---
+++ Inheritance.Net5.0/ElectricalDevice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.Net5._0
+{
+    internal class ElectricalDevice
+    {
+        //boolean to determine the state of the Electrical Device
+
+        public bool Ison { get; set; }
+        //string for the brand name of the Electrical Device
+
+        public string Brand { get; set; }
+
+        //basic constructor
+
+        public ElectricalDevice(bool isOn, string brand)
+        {
+
+            Ison = isOn;
+            Brand = brand;
+        }
+
+
+        //added methods
+        //switch on the Electrical Device
+
+        public void SwitchOn()
+        {
+            Ison = true;
+        }
+        //switch off the Electrical Device
+
+        public void SwitchOff()
+        {
+            Ison = false;
+        }
+    }
+}
